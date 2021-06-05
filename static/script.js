4dbd81130cba9962935a19eb5b100073fa010e48
I S <s3728388@student.rmit.edu.au>
2021-06-05 19:33:27 +1000

    completed profile modal

--- static/script.js
+++ static/script.js
@@ -1,41 +1,12 @@
-window.addEventListener('load', function () {
-  document.getElementById('sign-out').onclick = function () {
-    window.location.href = "logout";
-  };
-});
-
-function editpost(datetime){
-    var image = document.getElementById(datetime + "image");
-    var subject = document.getElementById(datetime + "subject").innerHTML;
-    var message = document.getElementById(datetime + "message").innerHTML;
-    console.log(image);
-    document.getElementById(datetime + "container").innerHTML =
-        '<form class="chat-box flex-row dark" enctype="multipart/form-data" action="edit_process" method="post">'+
-        '<div class="forum-form-body dark">'+
-            '<div style="justify-content: space-around;" class="flex-row container dark">'+
-                '<div class="">'+
-                    '<label style="margin: 5px;" for="id"><b>Subject</b></label>'+
-                    '<input type="text" placeholder="Subject" name="subject" value="' + subject + '" required>'+
-                '</div>'+
-                '<div>'+
-                    '<label style="margin: 5px;" for="id"><b>Message</b></label>'+
-                    '<input type="textarea" placeholder="Message..." name="message" value="' + message + '" required>'+
-                '</div>'+
-            '</div>'+
-            '<div class="flex-row container dark">'+
-                '<div class="">'+
-                    '<label style="margin: 5px;" for="img"><b>Attach Image</b></label>'+
-                    '<input type="file" id="img" name="file" accept="image/*">'+
-                '</div>'+
-                '<div style="display: grid;">'+
-                    '<label style="margin: 5px;" for="img"><b>or use previous image</b></label>'+
-                    '<img class="pfp-small" src="' + image.getAttribute('src') +'">'+
-                '</div>'+
-            '</div>'+
-        '</div>'+
-        '<input type="hidden" name="datetime" value="' + datetime + '">'+
-        '<div class="container forum-submit dark">'+
-            '<button class="forum-submit" type="submit">Update</button>'+
-        '</div>'+
-    '</form>';
+$(document).on("click", "#profile", function(data){
+    $("#profile-modal-outer").show()
 }
+)
+$(document).on("click", "#profile-modal-outer", function(data){
+    if(data.target.id=="profile-modal-outer"){
+        $("#profile-modal-outer").hide()
+    }   
+}
+)
+
+$(document).on("click", "#search", function(data){
